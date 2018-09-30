712dd6f9620ea1d081e150aae9c0a566625d46af
Matthew Lane <916179+lanoid@users.noreply.github.com>
2018-09-30 08:34:37 +0100

    🏭 Updating build process
    - Implemented Browser Sync
    - Fixed NPM Audit Issues
    - Updated Gulp
    - Created serve folder for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,60 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
+const gulp = require('gulp');
+const gulpLess = require('gulp-less');
+const gulpCopy = require('gulp-copy');
+const browserSync = require('browser-sync').create();
+const path = require('path');
 
-gulp.task('less', function () {
-  return gulp.src(['less/*.less', '!less/_*.less'])
-    .pipe(less({
-      paths: [ path.join(__dirname, '.', 'fonts/socicon') ]
+const paths = {
+  lessFiles: 'less/*.less',
+  lessBuildFiles: 'less/_*.less',
+  serve: 'serve/',
+  files: ['index.html', 'css/*.css', 'fonts/socicon/socicon-webfont.*']
+}
+
+function styles() {
+  return gulp.src([paths.lessFiles, `!${paths.lessBuildFiles}`])
+    .pipe(gulpLess({
+      paths: [path.join(__dirname, '.', 'fonts/socicon')],
+      javascriptEnabled: true
     }))
     .pipe(gulp.dest('css'));
-});
+}
 
-gulp.task('watch', function(){
-  gulp.watch('less/*.less', ['less']);
-});
+function watch() {
+  gulp.watch(paths.lessFiles, gulp.series(styles, build));
+}
 
-gulp.task('default', ['watch', 'less']);
+function build() {
+  return gulp.src(paths.files)
+    .pipe(gulpCopy(paths.serve))
+    .pipe(gulp.dest(paths.serve));
+}
+
+function cssReload(done) {
+  return gulp.src(`${paths.serve}/css/*`)
+    .pipe(browserSync.stream());
+}
+
+function dev() {
+
+  browserSync.init({
+    server: {
+      baseDir: paths.serve
+    },
+    files: paths.files,
+    injectChanges: true,
+    reloadOnRestart: true,
+    notify: false
+  });
+
+  gulp.watch(paths.lessFiles, gulp.series(styles, build, cssReload));
+  gulp.watch(paths.files).on('change', browserSync.reload);
+}
+
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+gulp.task('build', build);
+gulp.task('dev', dev);
+gulp.task('cssReload', cssReload);
+
+gulp.task('default', gulp.series(watch));
